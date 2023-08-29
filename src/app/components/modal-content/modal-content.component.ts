ef86b9c643197fb9ebbc6e14b0112d0d14730955
Suelany Brito <Suelanybr@gmail.com>
2023-08-29 00:36:35 -0300

    add modal

---
+++ src/app/components/modal-content/modal-content.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-content',
+  templateUrl: './modal-content.component.html',
+  styleUrls: ['./modal-content.component.scss']
+})
+export class ModalContentComponent {
+  @Input() modalTitle: string = 'Modal Título';
+  @Input() modalContent: string = 'Conteúdo do modal';
+  @Output() closeModal = new EventEmitter<void>();
+
+  onCloseModal() {
+    this.closeModal.emit();
+  }
+
+}
