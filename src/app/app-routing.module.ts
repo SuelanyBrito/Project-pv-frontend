4bca3256d86e9fe010510f4786d1bb62f0d12e77
Suelany Brito <Suelanybr@gmail.com>
2023-08-30 15:55:24 -0300

    change folder themes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ThemesComponent } from './pages/home/themes/themes.component';
+import { ThemesComponent } from './pages/themes/themes.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ThemesComponent } from './pages/home/themes/themes.component';
+import { ThemesComponent } from './pages/themes/themes.component';
 import { ModalContentComponent } from './components/modal-content/modal-content.component';
 
 @NgModule({
