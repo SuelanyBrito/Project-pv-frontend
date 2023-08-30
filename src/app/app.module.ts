62e13427dde031d4271ca25dc90e3e109fe5cd16
Suelany Brito <Suelanybr@gmail.com>
2023-08-30 15:53:21 -0300

    change folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ThemesComponent } from './components/themes/themes.component';
-import { ModalContentComponent } from './pages/modal-content/modal-content.component';
+import { ThemesComponent } from './pages/home/themes/themes.component';
+import { ModalContentComponent } from './components/modal-content/modal-content.component';
 
 @NgModule({
   declarations: [
